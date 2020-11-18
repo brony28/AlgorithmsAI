b63a7c63469c5d0fc10e5830b5238c64e21fde43
Rony Benny <37776233+brony28@users.noreply.github.com>
2020-11-19 02:09:58 +0530

    Update bfs_rbk.py

--- 01BFS/bfs_rbk.py
+++ 01BFS/bfs_rbk.py
@@ -1,21 +1,17 @@
-n = int(input("Enter no of nodes"))
+n = int(input("Enter no of nodes : "))
 
 g={}
 for i in range(n):
-    sNode = input("Enter node -> ")
-    cNodes = input("Enter its child node").split()
+    sNode = input("Enter node => ")
+    cNodes = input("Enter its child node => ").split()
     g[sNode] = cNodes
-    
-
     
 for key,val in g.items():
     print(f'{key} -> {val}')
 # print(g)
 
 
-
-
- class Nodes(object):
+class Nodes(object):
     
 #     orderOfNodes = []
     def __init__(self, treeDict =None):
@@ -70,28 +66,16 @@ for key,val in g.items():
         for val in queue:
             return self.findPath(val, goalNode, queue, orderOfPath, path, soluPathList, cost)
 
-
-
-
-
-
-
-
 n = Nodes(g)
 n.showNodes() # displays all nodes..
 
-
-
-
-startNode = input("Enter start node ")
-goalNode = input("Enter goal node ")
+startNode = input("Enter start node : ")
+goalNode = input("Enter goal node : ")
 queue = [startNode]
 print(queue)
 
-
 checker, ordor, solPath, cost = n.findPath(startNode,goalNode,queue) #displays
 print(checker)
-# solnPaa = n.solnPath(startNode, goalNode)
 print("Order of Nodes -> ",ordor)
 print("Solution Path -> ",solPath)
 print("Cost -> ",cost)
