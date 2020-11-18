62ea849beaf998c119634f247ee5281830b9f9c3
Rony Benny <37776233+brony28@users.noreply.github.com>
2020-11-19 02:10:53 +0530

    Update dfs_rbk.py

--- 02DFS/dfs_rbk.py
+++ 02DFS/dfs_rbk.py
@@ -1,22 +1,16 @@
-n = int(input("Enter no of nodes"))
+n = int(input("Enter no of nodes : "))
 
 g={}
 for i in range(n):
-    sNode = input("Enter node -> ")
-    cNodes = input("Enter its child node -> ").split()
+    sNode = input("Enter node => ")
+    cNodes = input("Enter its child node => ").split()
     g[sNode] = cNodes
-    
 
-    
 for key,val in g.items():
     print(f'{key} -> {val}')
 # print(g)
 
-
-
 class Nodes(object):
-    
-#     orderOfNodes = []
     def __init__(self, treeDict =None):
         
         if(treeDict is None):
@@ -26,9 +20,6 @@ class Nodes(object):
             
     def showNodes(self):
         return list(self.treeDict.keys())
-    
-
-
 
     def findPath(self, startNode, goalNode, stack, path=None, orderOfNodes = None,count = 0):
         if path == None:
@@ -39,7 +30,6 @@ class Nodes(object):
         path = path + [startNode]
         print(stack)
         temp = stack.pop(0)
-#         self.orderOfNodes.append(temp)
         orderOfNodes.append(temp)
         stack = tree[temp] + stack
         count+=1
@@ -54,18 +44,13 @@ class Nodes(object):
                     return extended_path
         return None
 
-
-
 n = Nodes(g)
 n.showNodes() # displays all nodes..
 
-
-
-startNode = input("Enter start node ")
-goalNode = input("Enter goal node ")
+startNode = input("Enter start node : ")
+goalNode = input("Enter goal node : ")
 stack = [startNode]
 print()
-
 
 solnPath, Cost, ordor= n.findPath(startNode,goalNode,stack) #displays
 print("Order of Nodes -> ",ordor)
